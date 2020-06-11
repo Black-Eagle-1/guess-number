0dd9bd695b5dc6f185a276fe494c8aff9ee089bd
Black Eagle <blackeagle9487@gmail.com>
2020-06-11 22:23:22 +0800

    add count

--- guessNumber.py
+++ guessNumber.py
@@ -1,12 +1,14 @@
 import random
 
 asw = random.randint(1, 100)
+count = 0
 
 while True:
 	number = input("請輸入號碼: ")
 	number = int(number)
+	count = count + 1
 	if asw == number:
-		print("恭喜答對")
+		print("恭喜答對  總共花費:", count, "次")
 		break
 	elif asw < number:
 		print("答錯，再嘗試看看  比", number, "小")
