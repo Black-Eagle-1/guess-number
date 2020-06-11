f9c1127006613b7e15652922c6560e01cfe0d15e
Black Eagle <blackeagle9487@gmail.com>
2020-06-11 23:41:21 +0800

    add modes

--- guessNumber.py
+++ guessNumber.py
@@ -1,24 +1,64 @@
 import random
 
-big = input("請輸入最大值: ")
-small = input("請輸入最小值: ")
-big = int(big)
-small = int(small)
+print("歡迎來到猜終極密碼")
+print("輸入1進入一般模式")
+print("輸入2進入自訂模式")
 
-asw = random.randint(small, big)
-#asw = random.randint(1, 100)
 
-count = 0
+
 
 while True:
-	number = input("請輸入號碼: ")
-	number = int(number)
-	count = count + 1
-	if asw == number:
+	mode = input("請選擇模式: ")
+	mode = int(mode)
+	count = 0
+
+	if mode == 1:
+		asw = random.randint(1, 100)
+		print("已進入一般模式")
+
+		number = input("請輸入號碼: ")
+		number = int(number)
+
+		while number != asw:
+			count = count + 1
+			if asw < number:
+				print("答錯，再嘗試看看  答案比", number, "小")
+				number = input("請輸入號碼: ")
+				number = int(number)
+			elif asw > number:
+				print("答錯，再嘗試看看  答案比", number, "大")
+				number = input("請輸入號碼: ")
+				number = int(number)
 		print("恭喜答對  總共花費:", count, "次")
 		break
-	elif asw < number:
-		print("答錯，再嘗試看看  比", number, "小")
-	elif asw >number:
-		print("答錯，再試看看 比", number, "大")
+
+
+	elif mode == 2:
+		big = input("請輸入範圍最大值: ")
+		small = input("請輸入範圍最小值: ")
+		big = int(big)
+		small = int(small)
+		asw = random.randint(small, big)
+		print("已進入自訂模式")
+
+		number = input("請輸入號碼: ")
+		number = int(number)
+		
+
+		while number != asw:
+			count = count + 1
+			if asw < number:
+				print("答錯，再嘗試看看  答案比", number, "小")
+				number = input("請輸入號碼: ")
+				number = int(number)
+			elif asw >number:
+				print("答錯，再嘗試看看  答案比", number, "大")
+				number = input("請輸入號碼: ")
+				number = int(number)
+		print("恭喜答對  總共花費:", count + 1, "次")
+		break
+
+	else:
+		print("只能輸入1或2")
+
 
